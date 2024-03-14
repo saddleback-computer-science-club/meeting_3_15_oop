7e80e764a0b889be360e2f85c34c3de02dc13307
kavehzare98 <66182975+kavehzare98@users.noreply.github.com>
2024-03-13 23:22:05 -0700

    creating first class

---
+++ Animal.h
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Animal 
+{
+private:
+    string coat;
+    string name;
+    int age;
+public:
+    Animal();
+    Animal(string coat, string n, int a);
+
+    void setCoat(string coat);
+    void setName(string n);
+    void setAge(int age);
+
+    string getCoat();
+    string getName();
+    string
+
+};
